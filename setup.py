86a45cf195d9945c6bf4baabf4b90747b91bd49a
SOCIALSCIENCEai <nick.sh.oh@socialscience.ai>
2024-01-08 20:38:37 +0000

    v0.1.6

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'redditharbor',
   packages = ['redditharbor'],   
-  version = '0.1.5.3',      
+  version = '0.1.6',      
   license='MIT',        
   description = 'A tool designed to effortlessly collect and store Reddit data in a Supabase database.',   
   author = 'Nick S.H Oh',                   
   author_email = 'nick.sh.oh@socialscience.ai',      
   url = 'https://github.com/socius-org/RedditHarbor/',  
-  download_url = 'https://github.com/socius-org/RedditHarbor/archive/refs/tags/0.1.5.3.tar.gz', 
+  download_url = 'https://github.com/socius-org/RedditHarbor/archive/refs/tags/0.1.6.tar.gz', 
   keywords = ['Reddit', 'Supabase', 'Crawler'],
   install_requires=[
           'praw == 7.7.1',
