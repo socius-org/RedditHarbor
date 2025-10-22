a4f2439abd812f9bda2cc871ea308af5a40a227f
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-10-22 06:11:13 +0100

    [app] Upgrade to Next.js 16 and remove Tailwind

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
