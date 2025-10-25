ec21fa7d09a5f36facea779da336b25a3639181f
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-10-26 00:53:08 +0100

    [app] Try to tighten up ESLint config

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import tseslint from 'typescript-eslint';
 
 const eslintConfig = defineConfig([
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   ...nextVitals,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   ...nextTs,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
