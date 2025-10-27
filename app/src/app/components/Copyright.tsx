50ec2f9db7be63dfbbef41d304c56b37604c8e06
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-10-27 05:31:12 +0000

    [app] Introduce MUI and remove Tailwind (#26)

---
+++ app/src/app/components/Copyright.tsx
@@ -0,0 +1,20 @@
+import Typography from '@mui/material/Typography';
+import MuiLink from '@mui/material/Link';
+
+export function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <MuiLink color="inherit" href="https://mui.com/">
+        Your Website
+      </MuiLink>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
---
+++ app/src/app/theme.ts
@@ -0,0 +1,10 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  colorSchemes: { light: true, dark: true },
+  cssVariables: true,
+  typography: {
+    fontFamily: 'var(--font-roboto)',
+  },
+});
