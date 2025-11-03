1e8da722e11d29460a8b1ccf147c65f20e94eb71
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-11-03 03:01:07 +0000

    [app] Try to tighten up ESLint config (#30)

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,10 +1,23 @@
+// @ts-check
+
 import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import tseslint from 'typescript-eslint';
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
   ...nextVitals,
   ...nextTs,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    // https://typescript-eslint.io/troubleshooting/typed-linting#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
+    files: ['eslint.config.mjs', 'postcss.config.mjs'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
