44b1f4c645b6b930fde4e30c0fe6029c1b9f09bb
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-11-07 23:53:33 +0000

    [app] Add app header (#34)

--- app/src/app/clerk.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-} from '@clerk/clerk-react';
---
+++ app/src/app/clerk.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useClerk } from '@clerk/clerk-react';
+import Button from '@mui/material/Button';
+
+export {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from '@clerk/clerk-react';
+
+export function SignOutButton() {
+  const { signOut } = useClerk();
+
+  return (
+    <Button
+      color="inherit"
+      size="small"
+      onClick={() => {
+        void signOut();
+      }}
+    >
+      Sign out
+    </Button>
+  );
+}
