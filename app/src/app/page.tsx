0dcaa340f5b89b691318050ca7cb31a3a88919a4
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-11-08 00:49:20 +0000

    [app] Remove MUI example content (#37)

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,31 +1,9 @@
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { ProTip } from '#app/components/ProTip.tsx';
-import { Copyright } from '#app/components/Copyright.tsx';
-import { Link } from '#app/components/Link.tsx';
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
+    <Container maxWidth="lg" sx={{ paddingY: 3 }}>
+      TODO
     </Container>
   );
 }
