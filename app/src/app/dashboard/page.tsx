cbdf1d8a584db7947b1aa7806d0aa85a1ad1a477
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-11-08 04:37:22 +0000

    [app] Redirect from `/` to `/dashboard` (#40)

---
+++ app/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Container from '@mui/material/Container';
+
+export default function Dashboard() {
+  return (
+    <Container maxWidth="lg" sx={{ py: 3 }}>
+      TODO
+    </Container>
+  );
+}
