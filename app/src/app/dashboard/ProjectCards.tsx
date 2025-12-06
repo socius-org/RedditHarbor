682043d0c8e0a5a5e82afedc262ee81071b2e74c
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-12-06 18:22:33 +0000

    [app] Render a project menu button

--- app/src/app/dashboard/ProjectCards.tsx
+++ app/src/app/dashboard/ProjectCards.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import type { SyntheticEvent } from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
 import type { Project } from '../actions/createProject';
 import { GridItem } from './GridItem';
 import { useProjects } from './useProjects';
@@ -13,15 +17,32 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }
 function ProjectCard({ project }: { project: Project }) {
   const { title, createdAt } = project;
 
+  function preventRipple(event: SyntheticEvent) {
+    event.stopPropagation();
+  }
+
   return (
     <Card sx={{ height: '100%' }}>
       <CardActionArea sx={{ height: '100%' }}>
-        <CardContent sx={{ height: '100%' }}>
-          <Typography variant="h6">{title}</Typography>
-          <Typography variant="body2" color="textSecondary">
-            Created {dateFormatter.format(new Date(createdAt))}
-          </Typography>
-        </CardContent>
+        <Stack sx={{ height: '100%' }}>
+          <CardHeader
+            action={
+              <IconButton
+                aria-label="Project menu"
+                onMouseDown={preventRipple}
+                onTouchStart={preventRipple}
+                onClick={() => {
+                  // TODO
+                }}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={title}
+            subheader={`Created ${dateFormatter.format(new Date(createdAt))}`}
+          />
+          <CardContent sx={{ flex: 1 }}>{/* TODO */}</CardContent>
+        </Stack>
       </CardActionArea>
     </Card>
   );
