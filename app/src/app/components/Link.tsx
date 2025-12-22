cc8b1e12283ae6b344908b32acdd0af7d51df0c3
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2025-12-22 05:16:10 +0000

    [app] Remove unused Link component (#184)

--- app/src/app/components/Link.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import MuiLink, { type LinkProps } from '@mui/material/Link';
-import NextLink from 'next/link';
-
-export function Link(props: LinkProps) {
-  return <MuiLink component={NextLink} {...props} />;
-}
