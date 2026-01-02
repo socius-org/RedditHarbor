06a576f94be27f35d78f146a88ce3531c4983cf6
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2026-01-02 04:08:09 +0000

    [app] Add `render` prop to `Card` (#215)

--- app/src/app/components/ui/card.tsx
+++ app/src/app/components/ui/card.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
+import { useRender } from '@base-ui/react/use-render';
 
 import { cn } from '#app/utils/cn.ts';
 
 function Card({
   className,
+  render,
   size = 'default',
   ...props
-}: React.ComponentProps<'div'> & { size?: 'default' | 'sm' }) {
-  return (
-    <div
-      data-slot="card"
-      data-size={size}
-      className={cn(
+}: useRender.ComponentProps<'div'> & { size?: 'default' | 'sm' }) {
+  return useRender({
+    defaultTagName: 'div',
+    render,
+    props: {
+      ...props,
+      className: cn(
         'ring-foreground/10 bg-card text-card-foreground gap-4 overflow-hidden rounded-xl py-4 text-sm ring-1 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col',
         className,
-      )}
-      {...props}
-    />
-  );
+      ),
+      'data-slot': 'card',
+      'data-size': size,
+    },
+  });
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
