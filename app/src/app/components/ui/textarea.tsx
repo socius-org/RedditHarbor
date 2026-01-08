23911a794ceab37ca8cac119c26c15598c269d58
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2026-01-08 06:21:24 +0000

    [app] Add `required` prop to `Field` component (#237)

--- app/src/app/components/ui/textarea.tsx
+++ app/src/app/components/ui/textarea.tsx
@@ -5,12 +5,13 @@ import type * as React from 'react';
 import { cn } from '#app/utils/cn.ts';
 import { useFieldContext } from '#app/components/ui/field.tsx';
 
-function Textarea({ className, id, ...props }: React.ComponentProps<'textarea'>) {
+function Textarea({ className, id, required, ...props }: React.ComponentProps<'textarea'>) {
   const fieldContext = useFieldContext();
 
   return (
     <textarea
       id={id ?? fieldContext?.id}
+      required={required ?? fieldContext?.required}
       data-slot="textarea"
       className={cn(
         'border-input dark:bg-input/30 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 disabled:bg-input/50 dark:disabled:bg-input/80 rounded-lg border bg-transparent px-2.5 py-2 text-base transition-colors focus-visible:ring-[3px] aria-invalid:ring-[3px] md:text-sm placeholder:text-muted-foreground flex field-sizing-content min-h-16 w-full outline-none disabled:cursor-not-allowed disabled:opacity-50',
